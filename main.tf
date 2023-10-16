f11d199c13498784f4663432534dc02895555d2a
Salih Furkan Demirer <salih.demirer@oredata.com>
2023-10-16 15:29:55 +0300

    modified main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 #   format = "DOCKER"
 # }
 
-resource "google_artifact_registry_repository" "test_repo_1" {}
+resource "google_artifact_registry_repository" "test_repo_1" {
   location = "europe-west1"
   repository_id = "test_repository_02!232.?**üği"
   format = "DDDDD"
