74ab8493cad0be6cfa8e87ea2c4e140204bb4c6a
Salih Furkan Demirer <salih.demirer@oredata.com>
2023-10-16 14:28:20 +0300

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "test_repo" {
   location = "europe-west1"
-  repository_id = "test-repository-02"
+  repository_id = "test_repository_02"
   format = "DOCKER"
 }
